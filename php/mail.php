fc6c894857e3d0f0b191d099f52600542c1cc887
AnthonyR <86236227+anthonyr-14051997@users.noreply.github.com>
2022-02-11 11:26:42 +0100

    php

--- php/mail.php
+++ php/mail.php
@@ -1,30 +1,46 @@
 
-<!-- ------------------------------------------------------------------------------- php -->
+
 <?php
-
-echo "cc", "\n";
-if (isset($_POST['btn'])) {
-    echo "cc";
-    $nom = $_POST['nom'];
-    $mail = $_POST['mail'];
-    $msg = $_POST['msg'];
-    $btn = $_POST['button'];
-    $all = $nom + $mail + $msg;
-    $retour = mail('aruby9905@gmail.com','test', $all);
-    echo $retour;
-    exit;
+/* Page: contact.php */
+//mettez ici votre adresse mail
+$VotreAdresseMail="aruby9905@gmail.com";
+// si le bouton "Envoyer" est cliqué
+if(isset($_POST['submit'])) {
+    //on vérifie que le champ mail est correctement rempli
+    if(empty($_POST['mail'])) {
+        echo "Le champ mail est vide";
+    } else {
+        //on vérifie que l'adresse est correcte
+        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['mail'])){
+            echo "L'adresse mail entrée est incorrecte";
+        }else{
+            //on vérifie que le champ sujet est correctement rempli
+            if(empty($_POST['nom'])) {
+                echo "Le champ prenom est vide";
+            }else{
+                //on vérifie que le champ message n'est pas vide
+                if(empty($_POST['msg'])) {
+                    echo "Le champ message est vide";
+                }else{
+                    //tout est correctement renseigné, on envoi le mail
+                    //on renseigne les entêtes de la fonction mail de PHP
+                    $Entetes = "MIME-Version: 1.0\r\n";
+                    $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
+                    $Entetes .= "From: Formulaire <".$_POST['mail'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
+                    $Entetes .= "Reply-To: Formulaire <".$_POST['mail'].">\r\n";
+                    //on prépare les champs:
+                    $Mail=$_POST['mail']; 
+                    $Sujet='=?UTF-8?B?'.base64_encode($_POST['nom']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
+                    $Message=htmlentities($_POST['msg'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
+                    //en fin, on envoi le mail
+                    if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){//la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
+                        echo "Le mail à été envoyé avec succès!";
+                    } else {
+                        echo "Une erreur est survenue, le mail n'a pas été envoyé";
+                    }
+                }
+            }
+        }
+    }
 }
-
 ?>
-
-
-<!--
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
-    require 'path/to/PHPMailer-master/src/Exception.php';
-    require 'path/to/PHPMailer-master/src/PHPMailer.php';
-    require 'path/to/PHPMailer-master/src/SMTP.php';
-
-    " phpmailer " !!!! 
--->
