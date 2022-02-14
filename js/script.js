08c2eb4121e1110cfc13b480d1e84c3761f957f9
AnthonyR <86236227+anthonyr-14051997@users.noreply.github.com>
2022-02-14 16:10:16 +0100

    js

--- js/script.js
+++ js/script.js
@@ -22,17 +22,26 @@ function send() {
     mail.style.border = "5px solid rgba(255, 255, 255, 0)";
     notnom.innerHTML = "";
     notmail.innerHTML = "";
+
+    /* ----------------------------------------------------- regex mail */
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     /* ----------------------------------------------------- nom vide */
     if ( nomval == '' ) {
         nom.style.border = "5px solid red";
-        notnom.innerHTML = "Le champs est vide !";
+        notnom.innerHTML = "Le champ est vide !";
         notnom.style.color = "red";
     } else
     /* ----------------------------------------------------- mail vide */
     if( mailval == '' ) {
         mail.style.border = "5px solid red";
-        notmail.innerHTML = "Le champs est vide !";
+        notmail.innerHTML = "Le champ est vide !";
+        notmail.style.color = "red";
+    } else
+    /* --------------------------------------------------- msg val */
+    if( msgval == '' ) {
+        mail.style.border = "5px solid red";
+        notmail.innerHTML = "Le champ est vide !";
         notmail.style.color = "red";
     } else
     /* ---------------------------------------------------- nom moins de deux caractères */
@@ -40,9 +49,8 @@ function send() {
         nom.style.border = "5px solid red";
         notnom.innerHTML = "Exemple : Max";
         notnom.style.color = "red";
-    }
+    } else
     /* --------------------------------------------------- mail incorrect */
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(String(mailval).toLowerCase()) == false) {
         mail.style.border = "5px solid red";
         notmail.innerHTML = "Exemple : exemple@gmail.com";
@@ -53,8 +61,12 @@ function send() {
         let sendmail = document.querySelector('.send');//recuperer l'icon du button
         
         btn.textContent = "Merci !";//remplacer le texte dans le button
+        /* req.responseType = 'json';
+            req.open('GET', url, true); */
 
         btn.appendChild(sendmail);//replacer l'icon dans le button
+
+        ajaxC(nomval , mailval,msgval  );
 
         return true //Envoie du formulaire
 
@@ -62,7 +74,32 @@ function send() {
     return false //refus d'envoie du formulaire
 }
 
+function ajaxC(nom,mail,msg){
+    /* let nomval = nom.value;
+    let mailval = mail.value;
+    let msgval = msg.value; */
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "./php/mail.php?submit=true&nom=" + nom + "&mail=" + mail + "&msg=" + msg);
 
+    if(xhr.status === 200) {
+        let response = document.querySelector("#mailValid");
+        response.innerHTML = "<p>" + xhr.responseText + "</p>";
+        if(xhr.responseText === "Votre mail a bien Ã©tÃ© envoyÃ© !"){
+            response.style.color = "#00bcd4";
+            response.style.border = "2px solid #00bcd4";
+        } else if(xhr.responseText === "Votre mail n'a pas pu Ãªtre envoyÃ©") {
+            response.style.color = "red";
+            response.style.border = "2px solid red";
+        } else {
+            response.style.color = "orange";
+            response.style.border = "2px solid orange";
+        }
+        response.style.display = "block";
+    } else {
+        console.log(xhr.status);
+    }
+    xhr.send();
+}
 
 /*
 
